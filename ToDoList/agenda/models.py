1e4ae613b6193f7df5b20f3caf2d465f9721acd6
kai <juho8582@gmail.com>
2023-05-11 00:01:01 +0900

    테이블에 author추가 (UserModel)이랑 연동

--- ToDoList/agenda/models.py
+++ ToDoList/agenda/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ToDos(models.Model):
     content = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete = models.CASCADE, default = '')
+
+
+
     
 
