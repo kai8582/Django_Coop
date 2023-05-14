dc28b1effd49e801e5270b8fd4b76a3f2b2bc028
eunekim <keh8992@gmail.com>
2023-05-15 01:06:15 +0900

    detail 수정

--- ToDoList/agenda/views.py
+++ ToDoList/agenda/views.py
@@ -37,9 +37,7 @@ def login(request):
             auth_login(request, user)
             return redirect('agenda:detail')
         else:
-            return redirect('agenda:login')
-           
-        
+            return redirect('agenda:login')    
     else:
         return render(request, 'agenda/login.html')
     
@@ -47,14 +45,14 @@ def logout(request):
     auth_logout(request)
     return redirect("agenda:index")
 
-
-
 @login_required(login_url = 'agenda:index')
-def details(request):
+def details(request, user):
     # 여기가 아마 진용님이 작업하실 뷰 인것같습니다. 여기서 초기화면 구현 로직 짜시면 됩니다! 저엿다면 objects.get(유저정보)로 데이터목록들을 불러올거같습니다!
     # url pattern과 function 이름도 제가 마음대로 햇으니 마음대로 수정하셔도 됩니다! 진용님파트가 제일 중요할거같네요ㅎㅎ HTML도 새로 만들어야될거같습니다. 추가로 settings.py파일에 login_redirect_url도 수정이 필요할거같습니다
     user = request.user
-    return render (request, 'agenda/details.html', {'user': user})
+    todos = ToDos.objects.get(user=user)
+    context = {'todos':todos}
+    return render (request, 'agenda/details.html', context)
 
 
 def update(request):
@@ -67,15 +65,15 @@ def delete(request):
 
 def create(request):
     if request.method == "POST":  
-        form = CreateForm(request.POST)
-        if form.is_valid():
-            todo_create = form.save(commit=False)
-            todo_create.content = request.POST.get('content')
-            todo_create.pub_data = timezone.now()
-            todo_create.author = request.user
-            todo_create.save()
+        form = CreateForm(request.POST) #폼객체 생성
+        if form.is_valid(): #데이터 유효성 체크
+            todo_create = form.save(commit=False) #폼객체를 통해 데이터 저장, commit=false는 데베저장 반영NO
+            todo_create.content = request.POST.get('content') #get 함수를 이용해 content를 저장
+            todo_create.pub_data = timezone.now() #현재시간 저장
+            todo_create.author = request.user #데이터 작성자 저장
+            todo_create.save() #데이터를 데베에 저장
         return redirect('agenda:index')
     else:
         form = CreateForm()
-    return render(request, 'agenda/createform.html')
+    return render(request, 'agenda/createform.html',)
     #은혜님 뷰! 제 생각에 createform.html 파일을 만들어야할거같습니다. 또 forms.py파일에 새로운 클래스와 데이터베이스 연동이 필요할듯합니다! 
