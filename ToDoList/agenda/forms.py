caf568c15cddb5cecfefe453c4a14a8b9a1fbd3b
khseon7 <gkrtjs5544@gmail.com>
2023-05-17 00:16:58 +0900

    주석 첨가 및 문서 확인

--- ToDoList/agenda/forms.py
+++ ToDoList/agenda/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-
+# 회원가입시 auth_user에 비밀번호는 암호화시켜서 저장, username은 그대로해서 저장한다.
 class UserForm(UserCreationForm):
     email=forms.EmailField(label="이메일")
     class Meta:
